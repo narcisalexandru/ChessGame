5adb1c1c034176ec05e0a7009f8e75c308289e94
Narcis Cazacu <cazacu.narcis02@gmail.com>
2024-05-16 10:59:34 +0300

    ITP_CG_3- fix pieces position

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -21,24 +21,34 @@ const getColor = (rowIndex: number, colIndex: number): 'white' | 'black' => {
 const getPiece = (rowIndex: number, colIndex: number): string | undefined => {
   // Piesele negre
   if (rowIndex === 0 && colIndex === 0) {
-    return '♜'; // Tura neagra
-  } else if (rowIndex === 0 && colIndex === 1) {
-    return '♞'; // Cal negru
-  } else if (rowIndex === 0 && colIndex === 2) {
-    return '♝'; // Nebun negru
-  } else if (rowIndex === 0 && colIndex === 3) {
-    return '♛'; // Regina neagra
-  } else if (rowIndex === 0 && colIndex === 4) {
-    return '♚'; // Regele negru
-  } else if (rowIndex === 0 && colIndex >= 5 && colIndex <= 6) {
-    return '♝'; // Al doilea nebun negru
-  } else if (rowIndex === 0 && colIndex >= 7) {
-    return '♞'; // Al doilea cal negru
-  } else if (rowIndex === 1) {
-    return '♟'; // Pionii negri
-
-  // Piesele albe
-  } else if (rowIndex === 7 && colIndex === 7) {
+    return '♜';
+  } 
+  else if (rowIndex === 0 && colIndex === 1) {
+    return '♞';
+  } 
+  else if (rowIndex === 0 && colIndex === 2) {
+    return '♝';
+  }
+  else if (rowIndex === 0 && colIndex === 3) {
+    return '♛'; 
+  }
+  else if (rowIndex === 0 && colIndex === 4) {
+    return '♚';
+  } 
+  else if (rowIndex === 0 && colIndex === 5) {
+    return '♝';
+  } 
+  else if (rowIndex === 0 && colIndex === 6) {
+    return '♞';
+  } 
+  else if (rowIndex === 0 && colIndex === 7) {
+    return '♜';
+  } 
+  else if (rowIndex === 1) {
+    return '♟';
+  }
+ // Piesele albe 
+  else if (rowIndex === 7 && colIndex === 7) {
     return '♖';
   } else if (rowIndex === 7 && colIndex === 6) {
     return '♘'; 
@@ -48,10 +58,12 @@ const getPiece = (rowIndex: number, colIndex: number): string | undefined => {
     return '♕'; 
   } else if (rowIndex === 7 && colIndex === 3) {
     return '♔'; 
-  } else if (rowIndex === 7 && colIndex <= 2) {
+  } else if (rowIndex === 7 && colIndex === 2) {
     return '♗'; 
-  } else if (rowIndex === 7 && colIndex <= 1) {
-    return '♘'; 
+  } else if (rowIndex === 7 && colIndex === 1) {
+    return '♘';
+  } else if (rowIndex === 7 && colIndex === 0) {
+    return '♖';
   } else if (rowIndex === 6) {
     return '♙'; 
   }
