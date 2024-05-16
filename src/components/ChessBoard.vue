1406eb9118a9da03c320d7d0eb4157dbcf33ff96
Narcis Cazacu <cazacu.narcis02@gmail.com>
2024-05-16 10:36:10 +0300

    add pieces on board

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,43 +1,75 @@
 <template>
-    <div class="board">
-      <div v-for="(row, rowIndex) in 8" :key="rowIndex" class="row">
-        <ChessCell
-          v-for="(col, colIndex) in 8"
-          :key="colIndex"
-          :color="getColor(rowIndex, colIndex)"
-        />
-      </div>
+  <div class="board">
+    <div v-for="(row, rowIndex) in 8" :key="rowIndex" class="row">
+      <ChessCell
+        v-for="(col, colIndex) in 8"
+        :key="colIndex"
+        :color="getColor(rowIndex, colIndex)"
+        :content="getPiece(rowIndex, colIndex)"
+      />
     </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent } from 'vue';
-  import ChessCell from '@/components/ChessCell.vue';
-  
-  export default defineComponent({
-    components: {
-      ChessCell
-    },
-    methods: {
-      getColor(rowIndex: number, colIndex: number): 'white' | 'black' {
-        // Dacă suma indexului rândului și al indexului coloanei este un număr par, returnăm 'white', altfel 'black'
-        return (rowIndex + colIndex) % 2 === 0 ? 'white' : 'black';
-      }
-    }
-  });
-  </script>
-  
-  <style scoped>
-  .board {
-    height: 100vh;
-    display: grid;
-    align-items: center;
-    justify-content: center;
-    align-content: center;
+  </div>
+</template>
+
+<script setup lang="ts">
+import ChessCell from '@/components/ChessCell.vue';
+
+const getColor = (rowIndex: number, colIndex: number): 'white' | 'black' => {
+  return (rowIndex + colIndex) % 2 === 0 ? 'white' : 'black';
+};
+
+const getPiece = (rowIndex: number, colIndex: number): string | undefined => {
+  // Piesele negre
+  if (rowIndex === 0 && colIndex === 0) {
+    return '♜'; // Tura neagra
+  } else if (rowIndex === 0 && colIndex === 1) {
+    return '♞'; // Cal negru
+  } else if (rowIndex === 0 && colIndex === 2) {
+    return '♝'; // Nebun negru
+  } else if (rowIndex === 0 && colIndex === 3) {
+    return '♛'; // Regina neagra
+  } else if (rowIndex === 0 && colIndex === 4) {
+    return '♚'; // Regele negru
+  } else if (rowIndex === 0 && colIndex >= 5 && colIndex <= 6) {
+    return '♝'; // Al doilea nebun negru
+  } else if (rowIndex === 0 && colIndex >= 7) {
+    return '♞'; // Al doilea cal negru
+  } else if (rowIndex === 1) {
+    return '♟'; // Pionii negri
+
+  // Piesele albe
+  } else if (rowIndex === 7 && colIndex === 7) {
+    return '♖';
+  } else if (rowIndex === 7 && colIndex === 6) {
+    return '♘'; 
+  } else if (rowIndex === 7 && colIndex === 5) {
+    return '♗'; 
+  } else if (rowIndex === 7 && colIndex === 4) {
+    return '♕'; 
+  } else if (rowIndex === 7 && colIndex === 3) {
+    return '♔'; 
+  } else if (rowIndex === 7 && colIndex <= 2) {
+    return '♗'; 
+  } else if (rowIndex === 7 && colIndex <= 1) {
+    return '♘'; 
+  } else if (rowIndex === 6) {
+    return '♙'; 
   }
   
-  .row {
-    display: flex; /* Așază celulele pe același rând */
-  }
-  </style>
-  
+  return undefined;
+};
+</script>
+
+<style scoped>
+.board {
+  height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+}
+
+.row {
+  display: flex; 
+}
+</style>
--- src/components/ChessCell.vue
+++ src/components/ChessCell.vue
@@ -1,23 +1,31 @@
 <template>
     <div :class="['cell', color]">
+      <div v-if="content" class="content">{{ content }}</div>
     </div>
 </template>
-
+  
 <script setup lang="ts">
-import { defineProps } from 'vue';
-
-const props = defineProps({
-    color: String as 'white' | 'black', // Specificăm tipul proprietății color
-    content: String
-});
+  const props = defineProps<{
+      color: 'white' | 'black',
+      content?: string
+  }>();
 </script>
-
+  
 <style>
 .cell {
     width: 100px;
     height: 100px;
     border: 1px solid black;
-}
+    position: relative;
+  }
+  
+.content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 24px;
+  }
 
 .white {
     background-color: #f0d9b5;
@@ -27,3 +35,4 @@ const props = defineProps({
     background-color: #b58863;
 }
 </style>
+  
