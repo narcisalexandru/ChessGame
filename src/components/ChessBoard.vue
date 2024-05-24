8ac5cdd7e0bc57416c4edff0520554defbc41f6f
Narcis Cazacu <cazacu.narcis02@gmail.com>
2024-05-24 19:32:40 +0300

    add board color change

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,25 +1,45 @@
 <template>
-  <div class="board">
-    <div v-for="(row, rowIndex) in board" :key="rowIndex" class="row">
-      <ChessCell
-        v-for="(cell, colIndex) in row"
-        :key="colIndex"
-        :color="getColor(rowIndex, colIndex)"
-        :content="cell"
-        :rowIndex="rowIndex"
-        :colIndex="colIndex"
-        @move-piece="movePiece"
-      />
+  <div>
+    <div class="theme-selector">
+      <label for="color-theme">Select theme:</label>
+      <select id="color-theme" v-model="selectedTheme">
+        <option v-for="(theme, name) in themes" :key="name" :value="name">{{ name }}</option>
+      </select>
+    </div>
+    
+    <div class="board">
+      <div v-for="(row, rowIndex) in board" :key="rowIndex" class="row">
+        <ChessCell
+          v-for="(cell, colIndex) in row"
+          :key="colIndex"
+          :color="getColor(rowIndex, colIndex)"
+          :content="cell"
+          :rowIndex="rowIndex"
+          :colIndex="colIndex"
+          @move-piece="movePiece"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import ChessCell from '@/components/ChessCell.vue';
 
-const getColor = (rowIndex: number, colIndex: number): 'white' | 'black' => {
-  return (rowIndex + colIndex) % 2 === 0 ? 'white' : 'black';
+const themes = {
+  'Default': { white: '#f0d9b5', black: '#b58863' },
+  'Green Theme': { white: '#a9bfa8', black: '#21571b' },
+  'Blue Theme': { white: '#add8e6', black: '#4682b4' },
+  'Red Theme': { white: '#f4c4c0', black: '#8a251e' },
+  'Pink Theme': { white: '#ffc0cb', black: '#cf1c7a' }
+};
+
+const selectedTheme = ref('Default');
+
+const getColor = (rowIndex: number, colIndex: number): string => {
+  const theme = themes[selectedTheme.value];
+  return (rowIndex + colIndex) % 2 === 0 ? theme.white : theme.black;
 };
 
 const initialBoard = [
@@ -45,8 +65,15 @@ const movePiece = (from: { row: number, col: number }, to: { row: number, col: n
 </script>
 
 <style scoped>
+.theme-selector {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 2rem;
+  gap: 5px;
+}
+
 .board {
-  height: 100vh;
   display: grid;
   align-items: center;
   justify-content: center;
--- src/components/ChessCell.vue
+++ src/components/ChessCell.vue
@@ -1,6 +1,7 @@
 <template>
   <div
-    :class="['cell', color]"
+    :class="['cell']"
+    :style="{ backgroundColor: color }"
     :draggable="!!content"
     @dragstart="onDragStart"
     @dragover.prevent
@@ -12,7 +13,7 @@
 
 <script setup lang="ts">
 const props = defineProps<{
-  color: 'white' | 'black',
+  color: string,
   content?: string,
   rowIndex: number,
   colIndex: number
@@ -50,13 +51,5 @@ const onDrop = (event: DragEvent) => {
   left: 50%;
   transform: translate(-50%, -50%);
   font-size: 60px;
-}
-
-.white {
-  background-color: #f0d9b5;
-}
-
-.black {
-  background-color: #b58863;
 }
 </style>
